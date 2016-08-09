5c9280496c573e7748b5826b18e7d05328236f68
Alice Lubic <lubicalice@gmail.com>
2016-08-09 13:53:54 -0400

    added lots of fun stuff but it crashes. to be continued

--- MapAndLocationApp/app/src/main/java/com/example/owlslubic/mapandlocationapp/MapsActivity.java
+++ MapAndLocationApp/app/src/main/java/com/example/owlslubic/mapandlocationapp/MapsActivity.java
@@ -10,19 +10,26 @@ import android.support.v4.content.ContextCompat;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+//    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
     private GoogleMap mMap;
     GoogleApiClient mApiClient = null;
     Location mLastLocation;
+//    LocationRequest mLocationRequest;
+    Marker mCurrLocationMarker;
 
 
 
@@ -36,9 +43,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
 
-
-
-
     }
 
     @Override
@@ -48,13 +52,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
 
-        //some sort of current location variable that i get from the Location objct
+
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
 
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            == PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
             //get instance of google api client
             if (mApiClient == null) {
                 mApiClient = new GoogleApiClient.Builder(this)
@@ -62,7 +66,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         .addOnConnectionFailedListener(this)
                         .addApi(LocationServices.API)
                         .build();
+                mApiClient.connect();
                 mMap.setMyLocationEnabled(true);
+
+            }
+
+
+                mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mApiClient);
+                if (mLastLocation != null) {
+
+                    //removed old marker just , now we place current marker
+        LatLng latLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng)
+                .title("Your Current Position")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+        mCurrLocationMarker = mMap.addMarker(markerOptions);
+
+        //move map camera
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+
 
             }
         }
@@ -91,23 +115,69 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
+
     @Override
     public void onConnected(@Nullable Bundle bundle) {
 
+        //gets quality of service for location updates from FusedLocationProviderApi using requestLocationUpdates
+        //not sure if i want or need this
+//        mLocationRequest = new LocationRequest();
+//        mLocationRequest.setInterval(1000)
+//                .setFastestInterval(1000)
+//                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+//        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+//            LocationServices.FusedLocationApi.requestLocationUpdates(mApiClient, mLocationRequest, this);
+//        }
 
-        //check permissions first
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mApiClient);
-        if(mLastLocation != null){
-            //can extract long and lat from the Location object returned by getLastLocation
-//            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
-//            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+
+//        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+//                == PackageManager.PERMISSION_GRANTED) {
+//            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mApiClient);
+//            if (mLastLocation != null) {
+//                //can extract long and lat from the Location object returned by getLastLocation
+////            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+////            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+//            }
+
         }
 
-    }
+
+
 
     @Override
     public void onConnectionSuspended(int i) {
 
     }
+
+
+    //this is something i saw in a tutorial and watned to try
+//    @Override
+//    public void onLocationChanged(Location location){
+//        mLastLocation = location;
+//        if(mCurrLocationMarker!=null){
+//            mCurrLocationMarker.remove();
+//        }
+//        //removed old marker, now we place current marker
+//        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+//        MarkerOptions markerOptions = new MarkerOptions();
+//        markerOptions.position(latLng)
+//                .title("Your Current Position")
+//                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+//        mCurrLocationMarker = mMap.addMarker(markerOptions);
+//
+//        //move map camera
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+//        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+//
+//        //stop location updates
+//        if(mApiClient != null){
+//          //  LocationServices.FusedLocationApi.removeLocationUpdates(mApiClient, this);
+//        }
+//
+//    }
+
+
+
+
 
 }
